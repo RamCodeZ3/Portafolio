96488b70abde34b01808f5ffcb017604a589e71d
RamCodeZ3 <arammusset7@gmail.com>
2025-08-10 15:57:35 -0400

    Se creo la seccion de start

---
+++ src/sections/start/Start.jsx
@@ -0,0 +1,26 @@
+function Start(){
+    return(
+        <>
+        <div className="flex w-full bg-gradient-to-r from-[#035f78] to-[#121e4e] p-0.5 rounded-lg">
+            <div className="flex bg-neutral-900/80 w-full items-center gap-12 px-20 py-5 rounded-lg">
+                <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2 text-6xl font-bold">
+                    <span>Hola, soy</span>
+                    <span class="bg-gradient-to-r from-[#035f78] to-[#121e4e] bg-clip-text text-transparent">
+                        Aram Musset
+                    </span>
+                </div>
+                <p className="w-140 text-lg">Soy programador Full-Stack, apasionado por la tecnología, siempre dispuesto a afrontar nuevos desafíos y enfocado en desarrollar proyectos de alta calidad, escalables y profesionales.</p>
+            </div>
+            <div className="bg-gradient-to-r from-[#035f78] to-[#121e4e] rounded-full p-2">
+                <div className="p-2 bg-neutral-900/80 rounded-full">
+                    <img src="/perfil.png" alt="" className="rounded-full size-98" />
+                </div>
+            </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Start;
