fa95d7ec9c4e0eb1f2206a7f92af31ab56c5aba4
RamCodeZ3 <arammusset7@gmail.com>
2025-08-14 21:55:05 -0400

    Se creo el archivo data de contactos

---
+++ src/sections/contatcs/Data.jsx
@@ -0,0 +1,20 @@
+const contatcsInfo = [
+    {
+        socialNetworking: 'Github',
+        description: 'Aqui podras ver todos mi proyectos',
+        link: 'https://github.com/RamCodeZ3',
+        icon: '',
+    },
+    {
+        socialNetworking: 'Whatsapp',
+        description: '',
+        link: '',
+        icon: '',
+    },
+    {
+        socialNetworking: 'Discord',
+        description: '',
+        link: '',
+        icon: '',
+    },
+]
