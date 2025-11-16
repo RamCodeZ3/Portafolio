57639a8ae8e4136d086c01b4138a67f024ca776c
RamCodeZ3 <arammusset7@gmail.com>
2025-11-15 21:17:31 -0400

    Se esta mejorando el portafolio primera parte header e inicio

--- src/sections/start/Start.tsx
+++ src/sections/start/Start.tsx
@@ -4,43 +4,40 @@ import StackTecnology from "./components/StackTecnology";
 import Buttons from "./components/Buttons";
 
 export default function Start(){
-    const [statulPerfil, setStatusPerfil] = useState(true)
     const { t } = useTranslation();
     
     return(
         <>
-        <div className="flex flex-col w-full justify-center items-center">
-            <div className="flex w-[100%] md:w-[90%] md:bg-gradient-to-r from-[#035f78] to-[#121e4e] md:p-0.5 md:rounded-lg">
-            <div className="flex flex-col lg:flex-row bg-neutral-900/40 md:bg-neutral-900/80 w-full items-center gap-12 px-6 lg:px-20 py-5 md:rounded-lg">
-                <div className="flex flex-col gap-4">
+        <div className="flex w-full md:p-0.5 md:rounded-lg">
+            <div className="flex flex-col bg-neutral-900/40 md:bg-neutral-900/80 w-full items-center gap-5 px-6 lg:px-20 py-5 md:rounded-lg">
+                <div className="flex items-center gap-30">
+                    <div className="flex flex-col gap-4">
                 <div className="flex flex-col gap-2 text-6xl font-bold">
-                    <span>{t('start.title')}</span>
-                    <span className="bg-gradient-to-r from-[#035f78] to-[#121e4e] bg-clip-text text-transparent">
-                        Aram Musset
+                    <span className="bg-[#0091b9] bg-clip-text text-transparent">
+                        Full-Stack
+                    </span>
+                    <span className="text-white">
+                        Developer
                     </span>
                 </div>
-                <p className="w-full lg:w-140 text-md md:text-lg">
+                <p className="w-full lg:w-140 text-md md:text-lg text-neutral-400">
                     {t('start.description')}
                 </p>
                 <Buttons/>
             </div>
             
-            <div className="bg-gradient-to-r from-[#035f78] to-[#121e4e] rounded-full p-2">
-                <div className="p-2 bg-neutral-900/80 rounded-full">
+            <div className="bg-[#0091b9] rounded-full p-2">
+                <div className="p-0.5 bg-white/20 rounded-full">
                     <img 
-                     src={statulPerfil? "/perfils/perfil.png": "/perfils/perfil2.png"} 
+                     src="/perfils/perfil2.png"
                      alt=""
-                     className="rounded-full sm:size-[60%] md:size-89 lg:size-auto cursor-pointer select-none"
-                     onClick={()=>{setStatusPerfil(!statulPerfil)}}
+                     className="rounded-full sm:size-[60%] md:size-69 lg:size-80 cursor-pointer select-none"
                      />
                 </div>
-            </div>
-            </div>
-            </div>
-            
-            <div>
+                </div>
+                </div>
                 <StackTecnology/>
-            </div>
+                </div>
         </div>
         </>
     )
