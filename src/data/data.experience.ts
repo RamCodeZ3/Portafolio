eff1a0679602e07581c73c74abd3741c459d79ee
RamCodeZ3 <arammusset7@gmail.com>
2026-02-15 23:00:41 -0400

    Se agrego en la data de experiencia la pagina de apuestas

--- src/data/data.experience.ts
+++ src/data/data.experience.ts
@@ -1,5 +1,12 @@
 export const exp = [
     {
+        img: "./screenshot/apuestas.webp",
+        description: "experience.description4",
+        rol: "Full-Stack Developer",
+        client: "Pagina de Sorteos",
+        tech: ["svg/TypeScript.svg", "svg/React-Dark.svg", "svg/TailwindCSS-Dark.svg", "svg/ExpressJS-Dark.svg", "svg/Supabase-Dark.svg", "svg/PostgreSQL.svg" ],
+    },
+    {
         img: "./screenshot/willcar.webp",
         description: "experience.description3",
         rol: "Full-Stack Developer",
